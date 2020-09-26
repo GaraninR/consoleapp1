21144911cf78c523534d0b87a4b17a3ab9a914e0
Roman <garanin.r@gmail.com>
2020-09-25 22:50:25 -0700

    Добавили класс Box и его вызов

---
+++ Box.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Box
+    {
+        int height;
+        int length;
+        int width;
+
+        public void setHeight(int param)
+        {
+            this.height = param;
+        }
+
+        public void setLength(int param)
+        {
+            this.length = param;
+        }
+
+        public void setWidth(int param)
+        {
+            this.width = param;
+        }
+
+        public int getVolume()
+        {
+            int volume = this.height * this.length * this.width;
+            return volume;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Box box = new Box();
+
+            box.setHeight(5);
+            box.setLength(4);
+            box.setWidth(2);
+
+            Console.WriteLine("Volume is {0}", box.getVolume());
+
         }
     }
 }
